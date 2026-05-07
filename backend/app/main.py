55d3f0e9227a17453378cca2e77960f118142e53
Sahil Yatam <sahilyatam9@gmail.com>
2026-05-07 17:52:48 +0530

    feat(app): integrate analytics routers into FastAPI application

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from app.db.db import db
 from app.core.config import get_settings
+from app.routers.user_router import router as user_router
+from app.routers.products_router import router as product_router
+from app.routers.orders_router import router as order_router
+from app.routers.revenue_router import router as revenue_router
 
 app = FastAPI()
 
 @app.get("/health")
 def health_check():
     return {"Status": "OK"}
+
+app.include_router(user_router)
+app.include_router(product_router)
+app.include_router(order_router)
+app.include_router(revenue_router)
 
 @app.on_event("startup")
 async def startup():
